e742da4665ec9411a6ee114ba8875bdf5b66dde1
Harsh Joshi <88710727+joshiharsh360@users.noreply.github.com>
2023-03-21 01:47:24 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  const fs = require("fs")
+  return (
+<View>
+  <Text>Test </Text>
+</View>
+    )
+}
+
+export default App
